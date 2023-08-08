815db183c2a17e977a0850be1d36daec27e3609e
Fabricio <fabriciolak12@gmail.com>
2023-08-08 10:33:17 -0300

    feat: add chat component

---
+++ src/components/Chat/index.tsx
@@ -0,0 +1,66 @@
+import NextImage from 'next/image'
+import React from 'react'
+
+type RootProps = React.ComponentProps<'div'>
+
+export function Root({ children, ...props }: RootProps) {
+  return (
+    <div className="flex w-80 gap-2 p-1 [&:not(:first-child)]:mt-4" {...props}>
+      {children}
+    </div>
+  )
+}
+
+type ControlProps = React.ComponentProps<'div'>
+
+export function Control(props: ControlProps) {
+  return (
+    <div className="flex-1 flex-col" {...props}>
+      {props.children}
+    </div>
+  )
+}
+
+type AboutProps = React.ComponentProps<'div'> & {
+  name: string
+  lastSeen: string
+}
+
+export function About({ name, lastSeen, children, ...props }: AboutProps) {
+  return (
+    <div className="flex h-11 items-center justify-between gap-2" {...props}>
+      <p className="w-48 flex-1 truncate text-base font-medium text-primary-gray">
+        {name}
+      </p>
+      <span className="text-xs font-medium text-tertiary-gray">{lastSeen}</span>
+    </div>
+  )
+}
+
+type ImageProps = React.ComponentProps<'div'> & {
+  imageURL: string
+}
+
+export function Image({ imageURL, ...props }: ImageProps) {
+  return (
+    <div className="h-11 w-11" {...props}>
+      <NextImage
+        className="rounded-full"
+        src={imageURL}
+        alt="Profile image"
+        width={42}
+        height={42}
+      />
+    </div>
+  )
+}
+
+type MessageProps = React.ComponentProps<'div'>
+
+export function Message({ children, ...props }: MessageProps) {
+  return (
+    <div className="border-b border-stroke pb-6" {...props}>
+      <p className="text-sm font-medium text-tertiary-gray">{children}</p>
+    </div>
+  )
+}
