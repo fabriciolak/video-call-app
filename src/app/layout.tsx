7aec56c185c30f1e5aba46d9ab80d188d0ee7adf
Fabricio <fabriciolak12@gmail.com>
2023-08-08 08:44:10 -0300

    feat: initial setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Video Call App',
+  description: 'Video call App by fabriciolak',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" className="antialiased">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div className="flex h-screen w-full flex-col items-center justify-center">
+      <h1 className="text-9xl font-bold">Jitter Social</h1>
+      <span className="mt-4 text-base font-medium">
+        A video call app by fabriciolak
+      </span>
+    </div>
+  )
+}
