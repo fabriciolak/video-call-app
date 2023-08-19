24939621f4f59510094b7910d8d08def22a68ec5
Fabricio <fabriciolak12@gmail.com>
2023-08-19 20:27:01 -0300

    chore: add some colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,32 @@ const config: Config = {
         'secondary-gray': '#6F7072',
         'tertiary-gray': '#888889',
         stroke: '#3F4346',
+
+        purple: '#AEA7D9',
+        yellow: 'EFB15F',
+        red: '#FD9485',
+        danger: '#DE3B46',
+        green: '#8AE261',
+
+        dark: '#121212',
+        'dark-500': '#292B2C',
+        'dark-600': '#2F3133',
+        'dark-700': '#1C1E20',
+
+        'title-color': '#DCDAE7',
+        background: '#101214',
+        blue: '#1C71D1',
+      },
+      width: {
+        container: 'min(1310px, 100%)',
+        'container-full': 'min(1630px, 100%)',
+      },
+      gridTemplateRows: {
+        video: '98px 1fr 7rem',
+      },
+      gridTemplateColumns: {
+        video: 'minmax(1fr, 1310px)',
+        'video-chat': '1fr 320px',
       },
     },
   },
