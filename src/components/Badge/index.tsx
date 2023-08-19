5b789a3f0a2590a4d4b5591abbdadcd9140b0376
Fabricio <fabriciolak12@gmail.com>
2023-08-18 22:11:23 -0300

    feat: add tw for classname

--- src/components/Badge/index.tsx
+++ src/components/Badge/index.tsx
@@ -1,3 +1,4 @@
+import { tw } from '@/lib/utils'
 import React from 'react'
 
 type RootProps = React.ComponentProps<'div'>
@@ -5,7 +6,10 @@ type RootProps = React.ComponentProps<'div'>
 export function Root(props: RootProps) {
   return (
     <div
-      className="inline-flex rounded-3xl bg-green px-6 py-2 text-xs font-semibold text-dark"
+      className={tw(
+        'inline-flex rounded-3xl px-6 py-2 text-xs font-semibold text-dark',
+        props.className,
+      )}
       {...props}
     >
       {props.children}
