b102483340b8ad84371e8d2a711eed1ba1586ebc
Fabricio <fabriciolak12@gmail.com>
2023-08-18 22:18:27 -0300

    refactor(lib): removed unnecessary clsx package from tw function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,14 @@
-import { clsx, type ClassValue } from 'clsx'
+import { v4 as uuidv4 } from 'uuid'
 import { twMerge } from 'tailwind-merge'
 
-export function tw(...inputs: ClassValue[]) {
-  return twMerge(clsx(...inputs))
+export function tw(...className: string[]): string {
+  return twMerge(...className)
+}
+
+export function generateUUID() {
+  const uuid = uuidv4()
+
+  console.log(uuid)
+
+  return uuid.slice(0, 4) + '-' + uuid.slice(4, 8) + '-' + uuid.slice(9, 13)
 }
